bf3466fd7a0a488ffe8e4edf9ade7c66c58d95ed
Stefan Nguyen <stefan@n3twork.com>
2019-08-04 07:46:49 +0700

    Implement Board.floodFill()

--- Source/core/Board.hx
+++ Source/core/Board.hx
@@ -2,15 +2,13 @@ package core;
 
 import haxe.ds.Vector;
 
+typedef Coordinate = { row:Int, col:Int }
 class Board {
     public static inline var NumRows = 10;
     public static inline var NumCols = 10;
 
-    struct Coordinate {
-        var row:Int;
-        var col:Int;
-    }
-
+    static var NeighborCoordinateDeltas = [ [0,1], [0,-1], [1, 0], [-1, 0] ];
+    
     var cellColors:Vector<Vector<CellColor>>;
 
     public function new() {
@@ -27,7 +25,9 @@ class Board {
 
     public function tryBlast(row:Int, col:Int):Bool {
         var connectedCells = floodFill(row, col);
-        return false;
+        trace('tryBlast($row, $col): color is ${getCellColor(row, col)}, connected cells: $connectedCells');
+        if (connectedCells.length < 2) return false;
+        return true;
     }
 
     public function toString():String {
@@ -59,12 +59,45 @@ class Board {
     }
 
     function validateCoordinate(row:Int, col:Int) {
-        if (row < 0 || row >= NumRows || col < 0 || col >= NumCols) {
-            throw 'Invalid board coordinate: $row, $col';
-        }
+        if (!isValidCoordinate(row, col)) throw 'Invalid board coordinate: $row, $col';
     }
 
-    // function floodFill(row:Int, col:Int):Array<Coordinate> {
-    //     new 
-    // }
+    function isValidCoordinate(row:Int, col:Int):Bool {
+        return !(row < 0 || row >= NumRows || col < 0 || col >= NumCols);
+    }
+
+    function floodFill(row:Int, col:Int):Array<Coordinate> {
+        var color = getCellColor(row, col);
+        var connectedCoors = new Array<Coordinate>();
+        var processingCoors = new Array<Coordinate>();
+        // A Set would be more optimal, but it's not built-in
+        var visitedLookup = new Map<Int, Bool>();
+
+        // Start at the given corodinate
+        processingCoors.push({ row: row, col: col });
+        visitedLookup.set(hashCoordinate(row, col), true);
+
+        while (processingCoors.length > 0) {
+            var coor = processingCoors.pop();
+            if (color == getCellColor(coor.row, coor.col)) {
+                connectedCoors.push(coor);
+                
+                // Check surrounding cells
+                for (delta in NeighborCoordinateDeltas) {
+                    var neighborRow = coor.row + delta[0];
+                    var neighborCol = coor.col + delta[1];
+                    var hashCode = hashCoordinate(neighborRow, neighborCol);
+                    if (isValidCoordinate(neighborRow, neighborCol) && !visitedLookup.exists(hashCode)) {
+                        processingCoors.push({ row: neighborRow, col: neighborCol });
+                        visitedLookup.set(hashCode, true);
+                    }
+                }
+            }
+        }
+        return connectedCoors;
+    }
+    
+    function hashCoordinate(row:Int, col:Int):Int {
+        return row * 100 + col;
+    }
 }
